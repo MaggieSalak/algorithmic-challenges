7229accd1a1beeb5cd01c66f4eb39ceaeffd63b5
MaggieSalak <39058899+MaggieSalak@users.noreply.github.com>
2020-08-04 16:42:00 +0200

    Update top_k_frequent_elements.cpp

--- leetcode/top_k_frequent_elements.cpp
+++ leetcode/top_k_frequent_elements.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> countPerVal;
-        map<int, unordered_set<int>> valuesPerCount;
+        unordered_map<int, int> numToCount;
+        map<int, unordered_set<int>> countToNums;
         
         for (const auto& n : nums) {
-            int count = countPerVal[n];
-            
-            if (valuesPerCount.find(count) != valuesPerCount.end())
-                valuesPerCount[count].erase(n);
-            
-            valuesPerCount[count+1].insert(n);
-            countPerVal[n]++;
-            
+            int count = ++numToCount[n];
+            countToNums[count].insert(n);
+            if (count > 1) {
+                countToNums[count-1].erase(n);
+            }
         }
+        
         vector<int> res;
-        auto it = valuesPerCount.rbegin();
-        int taken = 0;
-        while (taken < k) {
-            cout << it->first << endl;
-            auto s = it->second;
-            for (auto it1 = s.begin(); it1 != s.end(); ++it1) {
-                 if (taken == k) 
-                    break;
-                 res.push_back(*it1);
-                 ++taken;
-           }
-           ++it;
+        auto it = countToNums.rbegin();
+        
+        while (res.size() < k) {
+            for (const auto& n : it->second) {
+                if (res.size() < k) {
+                    res.push_back(n);
+                }
+            }
+            ++it;
         }
+        
         return res;
     }
 };
